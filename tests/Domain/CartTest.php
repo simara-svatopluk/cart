c68eaf95aab2e49293eccc2967624b5b0d2100e6
Svaťa Šimara <svatopluk.simara@gmail.com>
2017-08-21 22:50:09 +0200

    Add calculation tests

--- tests/Domain/CartTest.php
+++ tests/Domain/CartTest.php
@@ -16,4 +16,30 @@ class CartTest extends TestCase
 
         Assert::assertEquals($expected, $cart->calculate());
     }
+
+    public function testAddSingleProductToEmpty()
+    {
+        $cart = new Cart();
+        $cart->add('a', new Price(10.0));
+
+        $expectedItem = new DetailItem('a', new Price(10.0), 1);
+        $expected = new CartDetail([$expectedItem], new Price(10.0));
+
+        Assert::assertEquals($expected, $cart->calculate());
+    }
+
+    public function testAddTwoDifferentProducts()
+    {
+        $cart = new Cart();
+        $cart->add('a', new Price(10.0));
+        $cart->add('b', new Price(20.0), 2);
+
+        $expectedItems = [
+            new DetailItem('a', new Price(10.0), 1),
+            new DetailItem('b', new Price(20.0), 2),
+        ];
+        $expected = new CartDetail($expectedItems, new Price(50.0));
+
+        Assert::assertEquals($expected, $cart->calculate());
+    }
 }
