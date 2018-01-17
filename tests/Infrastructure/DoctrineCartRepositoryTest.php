87320d7ac312fac09a0c1f81c099f6b98e5a9183
Svaťa Šimara <svatopluk.simara@gmail.com>
2018-01-17 11:20:54 +0100

    Add a Doctrine specific cart repository test

--- tests/Infrastructure/DoctrineCartRepositoryTest.php
+++ tests/Infrastructure/DoctrineCartRepositoryTest.php
@@ -3,9 +3,11 @@
 namespace Simara\Cart\Infrastructure;
 
 use Doctrine\ORM\EntityManager;
+use PHPUnit\Framework\Assert;
 use Simara\Cart\Domain\Cart;
 use Simara\Cart\Domain\CartRepository;
 use Simara\Cart\Domain\Item;
+use Simara\Cart\Domain\Price;
 use Simara\Cart\Utils\EntityManagerFactory;
 use Simara\Cart\Utils\ConnectionManager;
 
@@ -39,5 +41,24 @@ class DoctrineCartRepositoryTest extends CartRepositoryTest
     {
         parent::tearDown();
         $this->entityManager->getConnection()->close();
+    }
+
+    public function testItemsAreRemovedWithCart()
+    {
+        $cart = new Cart('1');
+        $cart->add('1', new Price(10), 1);
+        $repository = $this->createRepository();
+        $repository->add($cart);
+        $this->flush();
+
+        $repository->remove('1');
+        $this->flush();
+
+        $queryBuilder = $this->entityManager->createQueryBuilder();
+        $queryBuilder->from(Item::class, 'i')
+            ->select('i');
+        $query = $queryBuilder->getQuery();
+        $result = $query->getResult();
+        Assert::assertCount(0, $result);
     }
 }
