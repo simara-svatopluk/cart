e0ed2de45a49661e4fb0b3d050189bfe6fafe3f8
Svaťa Šimara <svatopluk.simara@gmail.com>
2023-04-14 09:33:54 +0200

    Add DI configuration for real Doctrine setup

--- src/Infrastructure/Symfony/ContainerBuilderFactory.php
+++ src/Infrastructure/Symfony/ContainerBuilderFactory.php
@@ -4,24 +4,63 @@ declare(strict_types=1);
 
 namespace Simara\Cart\Infrastructure\Symfony;
 
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManager;
 use Simara\Cart\Application\CartUseCase;
 use Simara\Cart\Application\CartUseCaseApplication;
+use Simara\Cart\Domain\Cart\Cart;
 use Simara\Cart\Domain\Cart\CartRepository;
+use Simara\Cart\Domain\Cart\Item;
 use Simara\Cart\Domain\Prices\Prices;
 use Simara\Cart\Infrastructure\CsvPrices;
+use Simara\Cart\Infrastructure\DoctrineCartRepository;
 use Simara\Cart\Infrastructure\MemoryCartRepository;
+use Simara\Cart\Utils\ConnectionManager;
+use Simara\Cart\Utils\EntityManagerFactory;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 final class ContainerBuilderFactory
 {
     public static function createInMemory(
         string $pricesCsvPath,
-    ): ContainerBuilder
-    {
+    ): ContainerBuilder {
         $containerBuilder = new ContainerBuilder();
 
         $containerBuilder
             ->register(CartRepository::class, MemoryCartRepository::class);
+
+        $containerBuilder
+            ->register(Prices::class, CsvPrices::class)
+            ->addArgument($pricesCsvPath);
+
+        $containerBuilder
+            ->register(CartUseCase::class, CartUseCaseApplication::class)
+            ->setAutowired(true)
+            ->setPublic(true);
+
+        $containerBuilder->compile();
+
+        return $containerBuilder;
+    }
+    public static function create(
+        string $pricesCsvPath,
+    ): ContainerBuilder {
+        $containerBuilder = new ContainerBuilder();
+
+        $containerBuilder
+            ->register(Connection::class, Connection::class)
+            ->setFactory([ConnectionManager::class, 'createConnection']);
+
+        $containerBuilder
+            ->register(EntityManager::class, EntityManager::class)
+            ->setFactory([EntityManagerFactory::class, 'createEntityManager'])
+            ->setArgument('$schemaClassNames', [Cart::class, Item::class])
+            ->setArgument('$types', [])
+            ->setAutowired(true);
+
+        $containerBuilder
+            ->register(CartRepository::class, DoctrineCartRepository::class)
+            ->setAutowired(true);
 
         $containerBuilder
             ->register(Prices::class, CsvPrices::class)
--- tests/Infrastructure/DependenciesTest.php
+++ tests/Infrastructure/DependenciesTest.php
@@ -2,18 +2,27 @@
 
 namespace Simara\Cart\Infrastructure;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Simara\Cart\Application\CartUseCase;
 use Simara\Cart\Infrastructure\Symfony\ContainerBuilderFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 final class DependenciesTest extends TestCase
 {
-    public function testUseCaseCanBeObtained()
+    public static function containerBuilders()
     {
-        $containerBuilder = ContainerBuilderFactory::createInMemory(
+        yield [ContainerBuilderFactory::createInMemory(
             pricesCsvPath: __DIR__ . '/fixtures/prices.csv'
-        );
+        )];
+        yield [ContainerBuilderFactory::create(
+            pricesCsvPath: __DIR__ . '/fixtures/prices.csv'
+        )];
+    }
 
+    #[DataProvider('containerBuilders')]
+    public function testUseCaseCanBeObtained(ContainerBuilder $containerBuilder)
+    {
         $useCase = $containerBuilder->get(CartUseCase::class);
         $this->assertInstanceOf(CartUseCase::class, $useCase);
     }
